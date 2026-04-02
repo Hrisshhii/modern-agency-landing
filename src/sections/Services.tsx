3793d310b78205f5fe9de7cdf04e5873c5d631ea
Hrishi <rathodhrishi12345@gmail.com>
2026-04-03 00:21:17 +0530

    Adding the decorative image to the layout
    - Placed decorative image before text
    - Ensured text renders above using stacking order
    - Improved opacity for better readability

--- src/sections/Services.tsx
+++ src/sections/Services.tsx
@@ -2,11 +2,11 @@ import Container from "../components/Container";
 import underline from "../assets/underline.svg";
 import arrow from "../assets/arrow.png";
 import curve from "../assets/bg-line.png";
-
+import img from "../assets/img.png";
 const Services = () => {
   return (
     <section className="py-20 bg-[#f7f7f7] relative overflow-hidden">
-      <img src={curve} className="absolute right-[-28%] top-15 md:top-[-30%] w-[65%] opacity-80 pointer-events-none z-0"/>
+      <img src={curve} className="absolute right-[-28%] top-15 md:top-[-25%] w-[65%] opacity-80 pointer-events-none z-0"/>
       <Container>
         <div className="relative">
           <div className="mb-16 max-w-xl">
@@ -27,38 +27,44 @@ const Services = () => {
           <div className="divide-y divide-gray-300">
 
             <div className="py-6 flex items-center justify-between group">
-              <p className="text-xs text-gray-500 w-[25%]">
+              <p className="text-[0.75rem] md:text-[1.15rem] text-gray-500 w-[25%] md:w-[20%] mr-2 md:mr-0">
                 Office of multiple interest content
               </p>
-              <h3 className="font-gerbil text-lg md:text-3xl w-[50%]">
+              <h3 className="font-gerbil text-lg md:text-3xl w-[50%] px-2">
                 Collaborative & partnership
               </h3>
               <div className="w-[25%] flex justify-end">
-                <img src={arrow} className="w-16 transition group-hover:translate-x-2"/>
+                <img src={arrow} className="w-16 transition group-hover:translate-x-2 duration-500"/>
               </div>
             </div>
 
             <div className="py-6 flex items-center justify-between group">
-              <p className="text-xs text-gray-500 w-[25%]">
+              <p className="text-[0.75rem] md:text-[1.15rem] text-gray-500 w-[25%] md:w-[20%] mr-2 md:mr-0">
                 The hanger US Air force digital experimental
               </p>
-              <h3 className="font-gerbil text-lg md:text-3xl w-[50%]">
+              <h3 className="font-gerbil text-lg md:text-3xl w-[50%] px-2">
                 We talk about our weight
               </h3>
               <div className="w-[25%] flex justify-end">
-                <img src={arrow} className="w-16 transition group-hover:translate-x-2"/>
+                <img src={arrow} className="w-16 transition group-hover:translate-x-2 duration-500"/>
               </div>
             </div>
 
             <div className="py-6 flex items-center justify-between group">
-              <p className="text-xs text-gray-500 w-[25%]">
+              <p className="text-[0.75rem] md:text-[1.15rem] text-gray-500 w-[25%] md:w-[20%] mr-2 md:mr-0">
                 Delta faucet content, social, digital
               </p>
-              <h3 className="font-gerbil text-lg md:text-3xl w-[50%]">
-                Piloting digital confidence
+              <h3 className="relative font-gerbil text-lg md:text-3xl w-[50%] px-2">
+                Piloting digital{" "}
+                <span className="relative inline-block">
+                  <img src={img} className="absolute left-1/2 -translate-x-1/2 top-[50%] -translate-y-1/2 w-8 md:w-20 opacity-70 pointer-events-none"/>
+                  <span className="relative z-10">
+                    confidence
+                  </span>
+                </span>
               </h3>
               <div className="w-[25%] flex justify-end">
-                <img src={arrow} className="w-16 transition group-hover:translate-x-2 transition duration-300"/>
+                <img src={arrow} className="w-16 group-hover:translate-x-2 transition duration-500"/>
               </div>
             </div>
           </div>

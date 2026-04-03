de5bff8ebb0afcd19bf74efba4aac367bab386fb
Hrishi <rathodhrishi12345@gmail.com>
2026-04-03 20:45:03 +0530

    Fixing the quotation mark for mobile responsiveness

--- src/sections/Testimonial.tsx
+++ src/sections/Testimonial.tsx
@@ -23,13 +23,13 @@ const Testimonials = () => {
           <div className="max-w-xl mx-auto bg-[#e8efe9] rounded-3xl px-6 py-8 md:px-10 md:py-10 text-gray-700 relative">
 
             <p className="relative text-[0.85rem] md:text-base leading-[1.8] text-gray-700">
-              <span className="absolute left-0 top-[-10%] text-5xl md:text-8xl text-gray-500">“</span>
+              <span className="absolute left-[-5%] md:left-0 md:top-[-10%] text-5xl md:text-8xl text-gray-500">“</span>
               Elementum delivered the site within the timeline as they requested.
               In the end, the client found a 50% increase in traffic within days
               since its launch. They also had an impressive ability to use
               technologies that the company hasn&apos;t used, which have also proved
               to be easy to use and reliable
-              <span className="absolute right-[14%] -bottom-15 text-5xl md:text-8xl text-gray-500">”</span>
+              <span className="absolute right-[5%] md:right-[14%] md:-bottom-15 text-5xl md:text-8xl text-gray-500">”</span>
             </p>
           </div>
 
